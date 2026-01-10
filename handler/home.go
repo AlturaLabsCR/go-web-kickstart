f8de8bf0948010e562d1acab617c89622f38b857
tavo <gustavo.calvo.cr@gmail.com>
2026-01-10 17:57:03 -0600

    templates

--- handler/home.go
+++ handler/home.go
@@ -3,7 +3,9 @@ package handler
 import (
 	"net/http"
 
+	"app/config"
 	"app/config/routes"
+	"app/i18n"
 	"app/templates"
 	"app/templates/base"
 )
@@ -28,14 +30,27 @@ func (h *Handler) HomePage(w http.ResponseWriter, r *http.Request) {
 func (h *Handler) AboutPage(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
-	main := templates.HomeMain()
+	locale := ""
+
+	for _, lang := range i18n.RequestLanguages(r) {
+		if loc, ok := config.SupportedLocales[lang.Tag]; ok {
+			locale = loc
+			break
+		}
+	}
+
+	if locale == "" {
+		locale = config.SupportedLocales[config.DefaultLocale]
+	}
+
+	tr := h.Tr(r)
+
+	main := templates.AboutMain(tr, locale)
 
 	params := base.HeadParams{
 		LoadJS:      true,
 		RobotsIndex: true,
 	}
-
-	tr := h.Tr(r)
 
 	if err := base.Page(params, tr, main, routes.Map[routes.About]).Render(ctx, w); err != nil {
 		h.Log().Error("error rendering template", "error", err)
