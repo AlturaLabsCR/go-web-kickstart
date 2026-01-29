2eece125ba326689ae8d45004533af251d315abe
tavo <gustavo.calvo.cr@gmail.com>
2026-01-28 22:51:39 -0600

    translations

--- i18n/en.go
+++ i18n/en.go
@@ -3,5 +3,28 @@ package i18n
 const ENKey = "en"
 
 var EN = map[string]string{
-	"hello_world": "Hello, world!",
+	"nav.home":    "Home",
+	"nav.account": "Account",
+	"nav.about":   "About",
+
+	"protected.dashboard": "Dashboard",
+	"protected.title":     "Protected",
+
+	"protected.account": "Account",
+
+	"protected.settings":     "Settings",
+	"protected.manage_users": "Manage users",
+	"user.name":              "Username",
+	"user.perms":             "Permissions",
+	"session.last_used":      "Last used",
+
+	"protected.logout": "Log out",
+
+	"perm.admin":        "admin",
+	"perm.change_name":  "change name",
+	"perm.manage_users": "manage users",
+
+	"welcome":     "Welcome",
+	"device":      "Device",
+	"permissions": "Permissions",
 }
--- i18n/es.go
+++ i18n/es.go
@@ -3,5 +3,28 @@ package i18n
 const ESKey = "es"
 
 var ES = map[string]string{
-	"hello_world": "Hola, mundo!",
+	"nav.home":    "Inicio",
+	"nav.account": "Cuenta",
+	"nav.about":   "Acerca de",
+
+	"protected.dashboard": "Panel",
+	"protected.title":     "Protegido",
+
+	"protected.account": "Cuenta",
+
+	"protected.settings":     "Ajustes",
+	"protected.manage_users": "Gestionar usuarios",
+	"user.name":              "Nombre",
+	"user.perms":             "Permisos",
+	"session.last_used":      "Última actividad",
+
+	"protected.logout": "Salir",
+
+	"perm.admin":        "administrador",
+	"perm.change_name":  "cambiar nombre",
+	"perm.manage_users": "gestionar usuarios",
+
+	"welcome":     "Hola",
+	"device":      "Dispositivo",
+	"permissions": "Permisos",
 }
