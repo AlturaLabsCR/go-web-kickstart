0338f402e408b6c0affb0fec0e8f6d2aa6b94a69
tavo <gustavo.calvo.cr@gmail.com>
2026-01-28 22:22:32 -0600

    use production var

--- router/router.go
+++ router/router.go
@@ -20,10 +20,10 @@ func Init(h *handler.Handler, fs embed.FS) http.Handler {
 	// do not compress if log level <= 0 (debug mode), because:
 	// templ generate --notify-proxy requires the html response to inject
 	// the hot-reload script in development.
-	if config.Config.App.LogLevel < "0" {
-		globalMiddleware = middleware.Stack(middleware.Delay, h.LogRequest, middleware.ContentLength)
-	} else {
+	if config.Config.App.Env == config.ProductionEnv {
 		globalMiddleware = middleware.Stack(middleware.Gzip)
+	} else {
+		globalMiddleware = middleware.Stack(middleware.Delay, h.LogRequest, middleware.ContentLength)
 	}
 
 	return globalMiddleware(mux)
